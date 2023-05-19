0a0616bd0b713b68690b592e840f014ab2ecfdd0
ilyim <ilyimyan@gmail.com>
2023-05-19 13:12:27 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function palindrome(str) {
-    var sentence = str.replace(/[\W_]/g, '');
-    for (var i = 0, count = sentence.length - 1; i < count / 2; i++) {
-        if (sentence[i].toLowerCase() !== sentence[count - i].toLowerCase()) {
-            return false;
-        }
-    }
-    return true;
-};
+    event.preventDefault();
 
-palindrome("eYeye");
+    var str = document.getElementById('input').value;
+    var lowerAlpha = str.toLowerCase().replace(/[^a-z0-9]+/gi, "");
+    var lowerAlphaReverse = lowerAlpha.split('').reverse().join('');
+
+    if (lowerAlpha == lowerAlphaReverse && str.length > 0) {
+        results.innerHTML = 'is a palindrome';
+        document.getElementById("results").style.color = "white";
+        word.innerHTML = str;
+    } else {
+        results.innerHTML = 'is not a palindrome';
+        document.getElementById("results").style.color = "red";
+        word.innerHTML = str;
+    }
+}
